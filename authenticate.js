9f56f34282e75edb18a673e23247970f57371aa5
Danielle Robitshek <drobi710@gmail.com>
2021-04-26 19:17:53 -0400

    add admin auth

--- authenticate.js
+++ authenticate.js
@@ -35,3 +35,15 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate("jwt", { session: false });
+
+exports.verifyAdmin = function (req, res, next) {
+  console.log(req.user.admin);
+
+  if (req.user.admin) {
+    return next();
+  } else {
+    err = new Error(`You are not authorized to perform this operation!`);
+    err.status = 403;
+    return next(err);
+  }
+};
