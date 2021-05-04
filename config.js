3a93bb380484b9adf28afe9186622aea5f4c52a6
Danielle Robitshek <drobi710@gmail.com>
2021-05-04 18:57:53 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "520923772236497",
+    clientSecret: "a143fecabdf31d6b74d5d039238e91ae",
+  },
 };
